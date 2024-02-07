2a47345b57e990702dc1d57ec71c348c5d3c4ae5
Jubayed-A <learnigprograming@gmail.com>
2024-02-07 21:19:19 +0600

    signUp and Login completed

--- app/src/main/java/com/example/myapplication/LogInActivity.kt
+++ app/src/main/java/com/example/myapplication/LogInActivity.kt
@@ -4,17 +4,55 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityLogInBinding
+import com.example.myapplication.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LogInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLogInBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val signUp = findViewById<Button>(R.id.btnSigup)
+auth = FirebaseAuth.getInstance()
 
-        signUp.setOnClickListener {
-            val intentSigUp = Intent(this, SignUpActivity::class.java)
-            startActivity(intentSigUp)
+        binding.btnSignup.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish()
+        }
+
+        binding.apply {
+
+
+            btnLogIn.setOnClickListener {
+
+
+                val email = logInEmail.text.toString()
+                val password = logInPassword.text.toString()
+
+                if (email.isEmpty() && password.isEmpty()){
+                    Toast.makeText(this@LogInActivity, "Please fill the all filed", Toast.LENGTH_SHORT).show()
+                }else{
+                    auth.signInWithEmailAndPassword(email,password).addOnCompleteListener {task ->
+                        if (task.isSuccessful){
+                            startActivity(Intent(this@LogInActivity, MainActivity::class.java))
+                            finish()
+                            Toast.makeText(this@LogInActivity, "Login Successfully", Toast.LENGTH_SHORT).show()
+                        }else{
+                            Toast.makeText(this@LogInActivity, "LogIn Failed : ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+
+            }
+
+
         }
 
 
--- app/src/main/java/com/example/myapplication/SignUpActivity.kt
+++ app/src/main/java/com/example/myapplication/SignUpActivity.kt
@@ -1,11 +1,77 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+
+
+        binding.btnSignIn.setOnClickListener {
+            startActivity(Intent(this, LogInActivity::class.java))
+            finish()
+        }
+
+        binding.apply {
+            binding.btnSignUp.setOnClickListener {
+                val name = name.text.toString()
+                val email = email.text.toString()
+                val password = Password.text.toString()
+                val rePassword = rePassword.text.toString()
+
+                if (name.isEmpty() && email.isEmpty() && password.isEmpty() && rePassword.isEmpty()) {
+                    Toast.makeText(
+                        this@SignUpActivity,
+                        "Please File the all filed",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else if (password != rePassword) {
+                    Toast.makeText(
+                        this@SignUpActivity,
+                        "Password and RePassword Must be Same",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    auth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(
+                                    this@SignUpActivity,
+                                    "Registration is Successful",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                                startActivity(
+                                    Intent(
+                                        this@SignUpActivity,
+                                        LogInActivity::class.java
+                                    )
+                                )
+                                finish()
+                            } else {
+                                Toast.makeText(
+                                    this@SignUpActivity,
+                                    "Registration Failed : ${task.exception?.message}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                }
+            }
+        }
     }
 }
